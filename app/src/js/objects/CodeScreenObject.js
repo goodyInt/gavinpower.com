311f7a0495f115a493a870cb8cb7342aef32ef1d
goodyInt <gavin@goodyint.com>
2019-08-18 07:06:51 -0400

    updates to code copy

--- app/src/js/objects/CodeScreenObject.js
+++ app/src/js/objects/CodeScreenObject.js
@@ -46,37 +46,57 @@ function CodeScreen() {
   var thankYouDiv3 = document.createElement('div');
   thankYouDiv3.id = "thankYouDiv3";
   thankYouDiv3.className = "infoDiv";
-  thankYouDiv3.innerHTML = "<span class='infoTitle'>Khangeldy </span><span class='infoBody'>Thanks man. You are amazing and Three.js is amazing. Thank you. Thank you. Thanks man. You are amazing and Three.js is amazing. Thank you. Thank you.</span>";
+  thankYouDiv3.innerHTML = "<span class='infoTitle'>Khangeldy </span><span class='infoBody'>I don't know a lot about Khangeldy except that he lives in Almaty, Kazakhstan and he wrote the water shader that I manipulated in the StoryTelling section and graciiously shared it.</span>"+
+  "<br><span class='infoBody'>You can it out on <a href= https://codepen.io/Khangeldy/pen/pgXNMZ target='blank'> <span class='artistLink'>codepen</span></a> and his <a href= https://github.com/khangeldy target='blank'> <span class='artistLink'>github</span></a></span>";
+ 
+  
   codeScreenHolderDiv.appendChild(thankYouDiv3);
   codeScreenDivs.push(thankYouDiv3);
 
   var thankYouDiv4 = document.createElement('div');
   thankYouDiv4.id = "thankYouDiv4";
   thankYouDiv4.className = "infoDiv";
-  thankYouDiv4.innerHTML = "<span class='infoTitle'>Joshua Koo </span><span class='infoBody'>Thanks man. You are amazing and Three.js is amazing. Thank you. Thank you. </span>";
+  thankYouDiv4.innerHTML = "<span class='infoTitle'>Joshua Koo </span><span class='infoBody'>Joshua is an occasional three.js contributor and perpetuator of random crazy ideas. Organizes creative coding meetups in Singapore. He implimented the awesome sky shader (first implimented by Martin Upitis and improved by Simon Wallner) used in the last three sections.</span>"+
+  "<br><span class='infoBody'>Check out his <a href= https://www.linkedin.com/in/joshua-koo-8889b89/ target='blank'> <span class='artistLink'>linkedIn</span></a> and his <a href= https://twitter.com/blurspline target='blank'> <span class='artistLink'>twitter.</span></a></span>";
+ 
   codeScreenHolderDiv.appendChild(thankYouDiv4);
   codeScreenDivs.push(thankYouDiv4);
 
   var thankYouDiv5 = document.createElement('div');
   thankYouDiv5.id = "thankYouDiv5";
   thankYouDiv5.className = "infoDiv ";
-  thankYouDiv5.innerHTML = "<span class='infoTitle'>Conner Luzier </span><span class='infoBody'>Thanks man. You are amazing and Three.js is amazing. Thank you. Thank you. Thanks man. You are amazing and Three.js is amazing. Thank you. Thank you. Thanks man. You are amazing and Three.js is amazing. Thank you. Thank you. Thanks man. You are amazing and Three.js is amazing. Thank you. Thank you. Thanks man. You are amazing and Three.js is amazing. Thank you. Thank you. Thanks man. You are amazing and Three.js is amazing. Thank you. Thank you.</span>";
+  thankYouDiv5.innerHTML = "<span class='infoTitle'>Conner Luzier </span><span class='infoBody'>Conner was kind enough to post the model for the evergreen tree, from the Design Section, on CodePen. Thanks Conner.</span>" +
+  "<br><span class='infoBody'>Check out his <a href= https://codepen.io/cluzier/ target='blank'> <span class='artistLink'>CodePen.</span></a></span>";
+ 
   codeScreenHolderDiv.appendChild(thankYouDiv5);
   codeScreenDivs.push(thankYouDiv5);
 
   var thankYouDiv6 = document.createElement('div');
   thankYouDiv6.id = "thankYouDiv6";
   thankYouDiv6.className = "infoDiv";
-  thankYouDiv6.innerHTML = "<span class='infoTitle'>Victor Vergara </span><span class='infoBody'>Thanks man. You are amazing and Three.js is amazing. Thank you. Thank you. Thanks man. You are amazing and Three.js is amazing. Thank you. Thank you. Thanks man. You are amazing and Three.js is amazing. Thank you. Thank you. Thanks man. You are amazing and Three.js is amazing. Thank you. Thank you. Thanks man. You are amazing and Three.js is amazing. Thank you. Thank you. Thanks man. You are amazing and Three.js is amazing. Thank you. Thank you.</span>";
+  thankYouDiv6.innerHTML = "<span class='infoTitle'>Victor Vergara </span><span class='infoBody'>Victor is a visual designer from Columbia who enjoys exporting, creating and leading digital projects. His project Lab City 3D experiment was the starting point for the City Section. Thanks Victor.</span>" + 
+  "<br><span class='infoBody'>Check out his <a href= https://codepen.io/vcomics/ target='blank'> <span class='artistLink'>CodePen.</span></a></span>";
+ 
   codeScreenHolderDiv.appendChild(thankYouDiv6);
   codeScreenDivs.push(thankYouDiv6);
 
   var thankYouDiv7 = document.createElement('div');
   thankYouDiv7.id = "thankYouDiv7";
   thankYouDiv7.className = "infoDiv";
-  thankYouDiv7.innerHTML = "<span class='infoTitle'>David Hoskins </span><span class='infoBody'>Thanks man. You are amazing and Three.js is amazing. Thank you. Thank you. Thanks man. You are amazing and Three.js is amazing. Thank you. Thank you. Thanks man. You are amazing and Three.js is amazing. Thank you. Thank you. Thanks man. You are amazing and Three.js is amazing. Thank you. Thank you. Thanks man. You are amazing and Three.js is amazing. Thank you. Thank you. Thanks man. You are amazing and Three.js is amazing. Thank you. Thank you.</span>";
+  thankYouDiv7.innerHTML = "<span class='infoTitle'>David Hoskins </span><span class='infoBody'> Dave has been loving CG since the early 80s. And he is from England. He wrote the sick shader from the final, 'Thanks for Watching' section. He has written lots more too.</span>"+
+  "<br><span class='infoBody'>Check out his work - ridiculously awesome: <a href= https://www.shadertoy.com/user/Dave_Hoskins target='blank'> <span class='artistLink'>Shadertoy.</span></a></span>";
   codeScreenHolderDiv.appendChild(thankYouDiv7);
   codeScreenDivs.push(thankYouDiv7);
+
+
+  var thankYouDiv8 = document.createElement('div');
+  thankYouDiv8.id = "thankYouDiv8";
+  thankYouDiv8.className = "infoDiv";
+  thankYouDiv8.innerHTML = "<span class='infoTitle'>thisCode</span><span class='infoBody'>is available to download under an MIT License.</span>"+
+  "<br><span class='infoBody'>Check it out on <a href=https://github.com/goodyInt target='blank'> <span class='artistLink'>GitHub.</span></a></span><br><br><br>";
+ 
+  codeScreenHolderDiv.appendChild(thankYouDiv8);
+  codeScreenDivs.push(thankYouDiv8);
 
   var closeButtonDiv = document.createElement('div');
   closeButtonDiv.id = "closeButtonDivCodeScreen";
